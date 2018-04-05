1e1b774465a1909d2f2a9cac665bb5dfa3712013
DESKTOP-V7SBSQP\Jay <lenovo410>
2018-04-05 16:02:02 -0400

    add Availability is good now in doctor page

--- Clinic/Clinic/pages/AddAvailability.xaml.cs
+++ Clinic/Clinic/pages/AddAvailability.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using DAL;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,35 +22,79 @@ namespace Clinic
     /// </summary>
     public partial class AddAvailability : Page
     {
-        clinicEntities eContext = new clinicEntities();
-        CRUD crud = new CRUD();
+        clinicEntities context = new clinicEntities();
+        CollectionViewSource doctoravailabilitiesViewSource;
 
         public AddAvailability()
         {
             InitializeComponent();
-            
+            doctoravailabilitiesViewSource = ((CollectionViewSource)(this.FindResource("doctoravailabilitiesViewSource")));
         }
 
         private void btAdd_Click(object sender, RoutedEventArgs e)
         {
-            int doctorId = 5;
-            DateTime date = DateTime.Now.Date;
-            DateTime startTime = DateTime.Now.Date;
-            DateTime endTime= DateTime.Now.Date;
-            if (dpDateAddAv.Text != null) { date = Convert.ToDateTime(dpDateAddAv.Text); }
-            if (cmbStartTime.Text != null) { startTime = Convert.ToDateTime(cmbStartTime.Text); }
-            if (cmbEndTime.Text != null) { endTime = Convert.ToDateTime(cmbEndTime.Text); }
-            int duration = 0;
+
+            DateTime? date = null;
+            TimeSpan? startTime = null;
+            TimeSpan? endTime = null;
+            int duration;
+
+            if (!string.IsNullOrWhiteSpace(dpDateAddAv.Text)) { date = Convert.ToDateTime(dpDateAddAv.Text); }
+            if (!string.IsNullOrWhiteSpace(cmbStartTime.Text)) { startTime = TimeSpan.Parse(cmbStartTime.Text); }
+            if (!string.IsNullOrWhiteSpace(cmbEndTime.Text)) { endTime = TimeSpan.Parse(cmbEndTime.Text); }
+
             if (rb30M.IsChecked == true) { duration = 30; }
             else { duration = 60; }
 
-            availability av = new availability(doctorId, date, startTime, endTime, duration);
-            crud.AddAvailableTime(av);
-            MessageBox.Show("Added ", av.ToString());
-            dpDateAddAv.Text = "";
-            cmbStartTime.SelectedIndex = -1;
-            cmbEndTime.SelectedIndex = -1;
-         
+            //convert nullable DateTime to DateTime
+
+            DateTime DateToPass = (DateTime)(date);
+            DateTime sTime = (DateTime)(date + startTime);
+            DateTime eTime = (DateTime)(date + endTime);
+            //check the sTime if is overlapping with existing availabilities
+            bool CanAddAvailability = true;
+            var list = context.availabilities.Where(a => a.DoctorId == Globals.SessionId);
+            foreach (availability a in list)
+            {
+                if (sTime >= a.AvailableFrom && sTime <= a.AvailableTo) { CanAddAvailability = false; }
+
+            }
+            if (CanAddAvailability)
+            {
+                availability av = new availability(Globals.SessionId, DateToPass, sTime, eTime, duration);
+                context.availabilities.Add(av);
+                context.SaveChanges();
+                MessageBox.Show("Added ", av.ToString());
+                dpDateAddAv.Text = "";
+                cmbStartTime.SelectedIndex = -1;
+                cmbEndTime.SelectedIndex = -1;
+
+            }
+            if (!CanAddAvailability)
+            {
+                MessageBox.Show("Please check the entry, it is overlapping with existing availability");
+            }
+
         }
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            context.availabilities.Load();
+
+            doctoravailabilitiesViewSource.Source = context.availabilities.Local;
+            //add a filter to load only logged doctor availabilities
+            // doctor id obtained from log in SessionId
+            doctoravailabilitiesViewSource.View.Filter = item =>
+            {
+                availability m = item as availability;
+                if (m != null)
+                {
+                    if (m.DoctorId.Equals(Globals.SessionId))
+                        return true;
+                }
+                return false;
+            };
+        }
+
+        
     }
 }
